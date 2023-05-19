13225a1d0997a087fe7821016d9c38e637d7f2e4
bobur2006coder <boburdilmurodov702@gmail.com>
2023-05-19 14:50:06 +0500

    fix

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+let btn = document.querySelector(".sendBtn");
+
+
+
+
+let amalChalar = () => {
+    let answer = document.querySelector('.answer');
+    let amal = document.querySelector('.select-cha');
+    let oneInp = document.querySelector(".one-inp");
+    let twoInp = document.querySelector(".two-inp");
+    let one = Number(oneInp.value);
+    let two = Number(twoInp.value);
+    switch (amal.value) {
+        case "plus" :
+            answer.textContent=one+two
+            break;
+            case "minus" :
+                answer.textContent=one-two
+                break;
+                case "edvition" :
+                    answer.textContent=one+two
+                    break;
+                    case "increment" :
+                        answer.textContent=one+two
+                        break;
+        
+    }
+}
+
+btn.addEventListener("click", amalChalar)
